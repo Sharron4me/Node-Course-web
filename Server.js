e52b0611627b4dca42bf07fec9b8ca9ed505f454
Mohammad Ummair <mohdummair007@gmail.com>
2018-12-08 17:21:46 +0530

    Initial Commit

---
+++ Server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+var year =new Date().getFullYear();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs');
+app.use(express.static(__dirname + '/Public'));
+
+app.use((req,res,next)=>{
+  var log =  Date().toString() +` ${req.method} ${req.url}`;
+  fs.appendFile('Server.log',log+"\n",(err)=>{
+    if(err){
+      console.log("Error : Unable To append File!");
+    }
+  });
+  next();
+});
+
+// app.use((req,res,next)=>{
+//   res.render('Maintainence.hbs',{
+//     pageTitle:"Oops Sorry We are Working!"
+//   })
+// });
+
+hbs.registerHelper('currentYear',()=>{
+  return year
+});
+
+app.get('/', (req , res)=>{
+  //res.send('<h1>Hello World!</h1>');
+  res.render('Home.hbs',{
+    pageTitle:'Welcome To Home'
+  })
+});
+app.get('/about', (req , res)=>{
+  res.render('about.hbs',{
+    pageTitle:"Welcome To About"
+  });
+});
+app.get('/bad', (req , res)=>{
+  res.send({
+    Error:'Unable To process!'
+  });
+});
+
+app.listen(3000,()=>{
+  console.log("Here We start!");
+});
